761c088594de5240248fca269e428e9b54ea2930
mattzager <mzager@redhat.com>
2015-08-12 16:57:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -78,11 +78,10 @@ app.get('/hello', function(req, res) {
   
   var os = require('os');
   var interfaces = os.networkInterfaces();
-  var addresses = [];
   for (var k in interfaces) {
     for (var k2 in interfaces[k]) {
         var address = interfaces[k][k2];
-        if(address.startsWith('10.')) {
+        if(address.toString().startsWith('10.')) {
           res.write('Address: ' + address.address + '\n');
         }
     }
